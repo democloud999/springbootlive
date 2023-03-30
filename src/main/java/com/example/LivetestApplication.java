23ee4b6e14bbb239a93907c4469e4f23d292b762
cloudsony999 <cloudsony999@gmail.com>
2023-03-30 12:28:36 +0530

    first commit

---
+++ src/main/java/com/example/LivetestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivetestApplication.class, args);
+	}
+
+}
